a6956d05d4b28adbdc5e248ed0f377d927486638
Kevin wen <cybergodzilla1999@gmail.com>
2024-11-10 18:34:03 -0500

    multi ping responding 2

--- app/main.py
+++ app/main.py
@@ -10,7 +10,13 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     while True:
         connection, _ = server_socket.accept()
-        connection.sendall(b"+PONG\r\n")
+
+        while True:
+            data = connection.recv(1024)
+            if not data:
+                connection.close()
+                break
+            connection.sendall(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()
