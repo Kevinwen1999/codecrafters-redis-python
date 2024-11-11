157cf7d6d64e0b509ad471d255c47693f44ce7ef
Kevin wen <cybergodzilla1999@gmail.com>
2024-11-10 19:25:22 -0500

    Handle concurrent clients 1

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import socket  # noqa: F401
+import select
 
 
 def main():
@@ -8,15 +9,34 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    while True:
-        connection, _ = server_socket.accept()
+    server_socket.setblocking(False)
+    socket_list = [server_socket]
+    clients = {}
 
-        while True:
-            data = connection.recv(1024)
-            if not data:
-                connection.close()
-                break
-            connection.sendall(b"+PONG\r\n")
+    while True:
+        read_sockets, _, exception_sockets = select.select(socket_list, [], socket_list)
+
+        for notified_socket in read_sockets:
+            # new client socket:
+            if notified_socket == server_socket:
+                client_socket, client_address = server_socket.accept()
+                client_socket.setblocking(False)
+                socket_list.append(client_socket)
+                clients[client_socket] = client_address
+
+            else:
+                data = notified_socket.recv(1024)
+                if not data:
+                    socket_list.remove(notified_socket)
+                    del clients[notified_socket]
+                    notified_socket.close()
+                    continue
+                notified_socket.sendall(b"+PONG\r\n")
+
+        for notified_socket in exception_sockets:
+            socket_list.remove(notified_socket)
+            del clients[notified_socket]
+            notified_socket.close()
 
 if __name__ == "__main__":
     main()
